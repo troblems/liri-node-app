e519dc66e88c3c7114e9101165513fea37636794
Amanda Wittenstein <mashka.lewis@gmail.com>
2017-07-24 10:38:01 -0500

    movie push

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
+
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'ibA5kACygRtmYzpDg5GYy4tJ4',
+  consumer_secret: '<GCdCO66tx2ouEUA9rIEfbg9mNSXyJRK5zASf0HWyYklegYp5un>',
+  access_token_key: '<2914256546-ONRMUYxqqvg6KlYtRZ9y8S4Re9mxV0nN26yAjfi>',
+  access_token_secret: '<	sTHbyCKB2DcXI1ao9Ma6oJGiDelGzLJDycByS3ubb6oqu>',
 }
--- liri.js
+++ liri.js
@@ -1,8 +1,20 @@
-var key = require("keys.js");
+//needed NPM/files and variables
+var key = require("./keys.js");
 var request = require("request");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var movie;
+var song;
 
+//arguements from NPM
+var userInput = process.argv[2];
+var secondInput = process.argv[3];
 
-switch (action) {
+console.log("Pick your poison. my-tweets, spotify-this-song, movie-this, or do-what-it-says.");
+//switch cases
+function switchin(){
+  switch (userInput) {
   case "my-tweets":
     myTweets();
     break;
@@ -18,4 +30,44 @@ switch (action) {
   case "do-what-it-says":
     doWhat();
     break;
+  }
 }
+
+//running omdb api, Mr. Nobody if nothing is input, outputting whatever movie is input otherwise
+function movieThis(movie){
+  console.log("Lets watch a movie.");
+  if(secondInput===null) {
+  movie = "Mr. Nobody";
+  var query_url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=long&tomatoes=true&r=json";
+
+    //request is NPM package, query_url is variable error, res, body are apart of package.
+    request(query_url, function(error, res, body) {
+        //status code 200 is basically good to go.
+        if (!error && res.statusCode == 200) {
+            //list below from OMDB api
+            var movieInfo = {
+                "Title"                 : JSON.parse(body).Title,
+                "Released"              : JSON.parse(body).Released,
+                "Country"               : JSON.parse(body).Country,
+                "Languages"             : JSON.parse(body).Language,
+                "Actors"                : JSON.parse(body).Actors,
+                "IMDB Rating"           : JSON.parse(body).imdbRating,
+                "Rotten Tomatoes Rating": JSON.parse(body).tomatoRating,
+                "Rotten Tomatoes URL"   : JSON.parse(body).tomatoURL,
+                "Plot"                  : JSON.parse(body).Plot
+            };
+          };
+        });
+      };
+    };
+
+function doWhat(iWant){
+  console.log("");
+  fs.readfile("random.txt", "utf8", function (error, data) {
+  if(error) {
+    console.log(error)
+  }else{
+
+  };
+});
+};
